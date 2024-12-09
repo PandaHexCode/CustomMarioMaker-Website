92880aeb7d5fc1be4d9bc9d1a10f74495478ffcc
root <root@DESKTOP-BCR428N>
2024-12-09 13:04:44 +0100

    upload

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-const currentMonth = new Date().getMonth();
+const currentMonth = new Date().getMonth(); // Get the current month (0 = January, 11 = December)
 const logoElement = document.getElementById('logo');
 
 if (currentMonth === 11) {
     logoElement.src = 'logoWinter.png';
 }
+
+fetch('currentCMMVersion.txt')
+    .then(response => response.text())
+    .then(version => {
+        const downloadLink = document.getElementById('downloadLink');
+        downloadLink.innerHTML = `Download Alpha - ${version}`;
+    })
+    .catch(error => {
+        console.error('Error fetching version file:', error);
+    });
