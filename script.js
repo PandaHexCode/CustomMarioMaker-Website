f6c2e1843d4c3f8dcaaa1182344f957bed7299b2
root <root@DESKTOP-BCR428N>
2024-12-09 13:52:06 +0100

    upload

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ fetch('currentCMMVersion.txt')
     .catch(error => {
         console.error('Error fetching version file:', error);
     });
+
+const acc = document.getElementsByClassName("accordion");
+for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        const panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
